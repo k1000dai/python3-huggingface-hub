3794f069ec5571ffce76a64dca463eb5bab9e2c9
k1000dai <chiyodakku1000@gmail.com>
2025-03-12 20:18:15 +0000

    add debian dir with necessary dependencies

---
+++ debian/huggingface-hub.doc-base.ex
@@ -0,0 +1,20 @@
+Document: huggingface-hub
+Title: Debian huggingface-hub Manual
+Author: <insert document author here>
+Abstract: This manual describes what huggingface-hub is
+ and how it can be used to
+ manage online manuals on Debian systems.
+Section: unknown
+
+Format: debiandoc-sgml
+Files: /usr/share/doc/huggingface-hub/huggingface-hub.sgml.gz
+
+Format: postscript
+Files: /usr/share/doc/huggingface-hub/huggingface-hub.ps.gz
+
+Format: text
+Files: /usr/share/doc/huggingface-hub/huggingface-hub.text.gz
+
+Format: HTML
+Index: /usr/share/doc/huggingface-hub/html/index.html
+Files: /usr/share/doc/huggingface-hub/html/*.html
